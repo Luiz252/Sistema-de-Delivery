b457323a694188d877f8695c60c0434ef0c6ad0f
Lucas Evangelista <lucasilva127lucas@gmail.com>
2024-09-11 14:00:55 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    // Esconde a animação de introdução após 3 segundos
+   
     setTimeout(function() {
        document.getElementById('intro-animation').style.display = 'none';
        document.getElementById('main-login').style.display = 'flex';
